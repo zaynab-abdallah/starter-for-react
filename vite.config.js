84427206a81976295d68f7ff33be7c111d159319
zaynab-abdallah <zaynabshaltout718@gmail.com>
2025-10-24 18:27:19 +0300

    Remove duplicate base configuration in vite.config.js
    Signed-off-by: zaynab-abdallah <zaynabshaltout718@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/starter-for-react-movie/', // <--- هذا السطر مهم جدًا
   plugins: [react(), tailwindcss()],
-  base: '/starter-for-react-movie/',
 })
