ca88cfa0f6451bf5a38813397d6ff89344982324
zaynab-abdallah <zaynabshaltout718@gmail.com>
2025-10-24 19:09:33 +0300

    Update App.jsx
    Signed-off-by: zaynab-abdallah <zaynabshaltout718@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ const App = () => {
       <div className="pattern" />
       <div className="wrapper">
         <header>
-          <img src="./hero.png" alt="Hero Banner" />
+          <img src="./public/hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy
             Without the Hassle
