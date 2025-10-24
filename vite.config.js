a660a9be8e70d1808732dbf3e574490e1774a550
zaynab-abdallah <zaynabshaltout718@gmail.com>
2025-10-24 19:02:26 +0300

    Update vite.config.js
    Signed-off-by: zaynab-abdallah <zaynabshaltout718@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/starter-for-react-movie/', // <--- هذا السطر مهم جدًا
+  base: '/starter-for-react-movie/', 
   plugins: [react(), tailwindcss()],
 })
